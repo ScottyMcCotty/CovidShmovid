ed1ae5a44d53aa381758fa46ea5c30ddbcae8e69
ScottyMcCotty <scottwillgotocollege@gmail.com>
2022-02-19 01:06:25 -0800

    Add a placeholder page

--- js/information.js
+++ js/information.js
@@ -3,14 +3,24 @@ import {Page} from "./page.js"
 
 class Information extends Page
 {
-  constructor() {
+  constructor(information_text) {
     super()
     // a page purely for giving information to the user
+    this.information_text = information_text;
   }
 
   activate() {
-    console.log("Information trying to activate()");
+    console.log("Activating a new Information");
+    document.getElementById("button container").hidden = true;
+    document.getElementById("question label").innerText = this.information_text;
   }
+
+  static PLACEHOLDER() {
+    let placeholder = new Information("Blank Page!\n\n\
+                                       This page is a placeholder for future pages\n\n\
+                                       Replace with a question or information page");
+    return placeholder;
+  } 
 }
 
 export { Information }
