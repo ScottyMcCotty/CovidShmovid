2ed5b94ae2894ba8facadd1aac49d63a790bcb90
ScottyMcCotty <scottwillgotocollege@gmail.com>
2022-02-19 01:05:30 -0800

    Add method for setting next node pointers
    This is better because when the pointer is saved, the child's parent reference can be set as well

--- js/question.js
+++ js/question.js
@@ -1,5 +1,6 @@
 
 import {Page} from "./page.js"
+import {Information} from "./information.js"
 
 // this class acts as a Node in our tree
 class Question extends Page
@@ -9,8 +10,9 @@ class Question extends Page
   // question text
   // answer button 1 text
   // answer button 2 text
-  // the next Question when answer 1 is pressed
-  // the next Question when answer 2 is pressed
+  // the previous Page
+  // the next Page when answer 1 is pressed
+  // the next Page when answer 2 is pressed
 
   constructor (question_text, answer_1, answer_2) {
     super()
@@ -20,11 +22,11 @@ class Question extends Page
 
     // using null values here isn't great design...
     // ideally we could have an empty question instance or something
-    this.next_question_1 = null
-    this.next_question_2 = null
+    this.set_next_1(Information.PLACEHOLDER());
+    this.set_next_2(Information.PLACEHOLDER());
 
     // also keep track of the parent Question instance
-    this.previous_question = null
+    this.previous = null
   }
 
   activate () {
@@ -34,6 +36,16 @@ class Question extends Page
     document.getElementById("answer 1").value = this.answer_button_1_label
     document.getElementById("answer 2").value = this.answer_button_2_label
     document.getElementById("question label").innerText = this.question_text
+  }
+
+  set_next_1 (reference_to_next_page) {
+    this.next_1 = reference_to_next_page;
+    reference_to_next_page.previous = this;
+  }
+
+  set_next_2 (reference_to_next_page) {
+    this.next_2 = reference_to_next_page;
+    reference_to_next_page.previous = this;
   }
 }
 
